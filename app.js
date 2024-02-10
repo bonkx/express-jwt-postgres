49a9f0b7f93430733ddb55530fb77224af14f4da
farridputra@gmail.com <farridputra@gmail.com>
2024-02-10 22:07:06 +0700

    add eslint

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
+// dotenv at the top
+require('dotenv').config()
+require('express-group-routes');
+
 var createError = require('http-errors');
+var compression = require('compression');
+var helmet = require("helmet");
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var favicon = require('serve-favicon');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var authRouter = require('./routes/auth');
+
+var ErrorHandler = require('./middlewares/ErrorHandler');
 
 var app = express();
+app.locals.env = process.env;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,24 +29,52 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.png')));
 
+app.use(compression()); // Compress all routes
+
+// Add helmet to the middleware chain.
+// Set CSP headers to allow our Bootstrap and Jquery to be served
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  }),
+);
+
+// Reduce Fingerprinting
+app.disable('x-powered-by')
+
+// ROUTES
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+app.group("/api/v1", (router) => {
+  // PUBLIC ROUTES
+  router.use('/auth', authRouter);
+
+  // MIDDLEWARE ROUTES
+  router.use('/users', usersRouter);
+});
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+/**
+ * Any error handler middleware must be added AFTER you define your routes.
+ */
+app.use(ErrorHandler);
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 module.exports = app;
