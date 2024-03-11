58678774eedc7490f827b3529a82ac584f15d929
farridputra@gmail.com <farridputra@gmail.com>
2024-03-11 19:41:48 +0700

    add model otp_requests

--- src/entity/seeders/20240214134145-users.js
+++ src/entity/seeders/20240214134145-users.js
@@ -5,24 +5,24 @@ const bcrypt = require('bcryptjs');
 
 module.exports = {
     async up(queryInterface, Sequelize) {
-        // return queryInterface.bulkInsert('users', [
-        //     {
-        //         username: 'admin',
-        //         email: 'admin@admin.com',
-        //         first_name: 'Admin',
-        //         last_name: 'Adm',
-        //         phone_number: '+6281234567890',
-        //         password: bcrypt.hashSync(process.env.ADMIN_PASS, 12),
-        //         active: true,
-        //         role: 'admin,
-        //         created_at: new Date(),
-        //         updated_at: new Date(),
-        //     },
-        // ]);
+        return queryInterface.bulkInsert('users', [
+            {
+                username: 'admin',
+                email: 'admin@admin.com',
+                first_name: 'Admin',
+                last_name: 'Adm',
+                phone_number: '+6281234567890',
+                password: bcrypt.hashSync(process.env.ADMIN_PASS, 12),
+                active: true,
+                role: 'admin',
+                created_at: new Date(),
+                updated_at: new Date(),
+            },
+        ]);
     },
 
     async down(queryInterface, Sequelize) {
         // return await queryInterface.dropTable('users', { restartIdentity: true });
-        // return queryInterface.bulkDelete('users', { username: 'admin' }, {});
+        return queryInterface.bulkDelete('users', { username: 'admin' }, {});
     },
 };
